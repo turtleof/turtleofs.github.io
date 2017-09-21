1279ceda8ff022461214b93fbeceb4d5d421447e
turtleof <taske.hlp@gmail.com>
2017-09-21 23:24:16 +0400

    create slider in works

--- js/bundle.js
+++ js/bundle.js
@@ -23,14 +23,68 @@ $(function() {
 		$('.buttons').slideToggle(300, function() {
 
 			if($(this).css('display') === 'none') {
+
 				$(this).removeAttr('style');
-
 			}
-
 		})
-
 	}); 
-
 });
+
+
+
+$(function() {
+	var slideNow = 1;
+	var slideCount = $('.slide__area').children().length;
+	var translateWidth = 0;
+
+	function nextSlide() {
+		if(slideNow == slideCount || slideNow <= 0 || slideNow > slideCount){
+			$('.slide__area').css('transform', 'translate(0, 0)');
+			slideNow = 1;
+		}
+		else {
+			translateWidth = -$('.viewport').width() * (slideNow);
+			$('.slide__area').css({
+				'transform': 'translate(' + translateWidth + 'px, 0)',
+            	'-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            	'-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+			});
+			slideNow++;
+		}
+	}
+
+	function prevSlide() {
+		if(slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+			translateWidth = -$('.viewport').width() * (slideCount - 1);
+			$('.slide__area').css({
+				'transform': 'translate(' + translateWidth + 'px, 0)',
+            	'-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            	'-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+			});
+			slideNow = slideCount;
+			}
+			else {
+				translateWidth = -$('.viewport').width() * (slideNow - 2);
+				$('.slide__area').css({
+					'transform': 'translate(' + translateWidth + 'px, 0)',
+            	'-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            	'-ms-transform': 'translate(' + translateWidth + 'px, 0)'
+				});
+				slideNow--;
+			}
+	}
+
+	
+
+	$('#prev').on('click', function(){
+		prevSlide();
+		
+	});
+
+	$('#next').click(function(){
+		nextSlide();
+	})
+});
+
 
 
